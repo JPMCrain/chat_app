83dea4040103526a892701a00a181ab27c80c897
Jean-Paul-Michael Crain <jpmcrain@gmail.com>
2019-06-30 02:53:42 -0900

    added function to change the star from filled to unfilled in app bar

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
+let star = document.getElementById('starImage');
+
 function switchChannelName(channelName, channelLocation) {
 	document.getElementById('channelName').innerHTML = channelName;
 	document.getElementById('channelLocation').innerHTML = `by: ${channelLocation}`;
 	document.getElementById('channelLocation').href = `http://what3words.com/${channelLocation}`;
+	star.src = 'https://ip.lfe.mw.tum.de/sections/star-o.png';
 }
 
 
 function switchFavoChannel(){
-	
+	if(star.src == 'https://ip.lfe.mw.tum.de/sections/star-o.png'){
+		star.src = 'https://ip.lfe.mw.tum.de/sections/star.png';
+	}else{
+		star.src = 'https://ip.lfe.mw.tum.de/sections/star-o.png';
+	}
 }
